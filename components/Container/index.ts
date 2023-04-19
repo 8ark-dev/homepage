3c0d3fb74c7dd6b7fa6ba11faa235822d5c947e3
8ark <sorlti6952@gmail.com>
2023-04-19 10:39:11 +0900

    init2
    init2

---
+++ components/Container/index.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div({
+    width: '100%',
+    height: '100%',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+});
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Head from 'next/head'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        <meta name="description" content="A site for my programming portfolio" />
+        <meta name="og:title" content="whale homepage" />
+        
+        <title>새 탭</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>)
 }
