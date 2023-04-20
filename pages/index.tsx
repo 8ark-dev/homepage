433b6811cc5c60e71071b57723cdfa08d993d454
8ark-dev <sorlti6952@gmail.com>
2023-04-20 21:37:01 +0900

    FECH dayjs
    add date

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,95 @@
-import classNames from 'classnames'
-import { useEffect, useState } from 'react'
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
-import { Container } from '../components/Container'
-import styled from '@emotion/styled'
-import dayjs from 'dayjs'
+import { Container } from '../components/Container';
+import styled from '@emotion/styled';
+import dayjs from 'dayjs';
+
+const enToday = {
+  Mon: '월요일',
+  Tue: '화요일',
+  Wed: '수요일',
+  Thu: '목요일',
+  Fri: '금요일',
+  Sat: '토요일',
+  Sun: '일요일',
+};
 
 export default function Home() {
-  const [timer, setTimer] = useState(123);
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setTimer((prev) => prev + 1)
-  //   }, 1000);
+  const [timer, setTimer] = useState(dayjs().format('YYYY-MM-DD ddd HH:mm:ss'));
 
-  //   return () => {
-  //     clearInterval(timer);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setTimer(dayjs().format('YYYY-MM-DD ddd HH:mm:ss'));
+    }, 1000);
+
+    //console.log('timer:', timer.split(' '));
+    return () => {
+      clearInterval(counter);
+    };
+  }, []);
 
   return (
     <BaseContainer>
-      <div className='background' />
+      <div className="background" />
       {/* 상단 우측 사용 의견 */}
-      <div>
-
-      </div>
+      <div></div>
 
       {/* 가운데 메인 콘텐츠 */}
       <div className={classNames('mid-container')}>
         {/* 검색 창 */}
         <SearchBox>
-
-          <input
-            type="text"
-            placeholder="검색어를 입력하세요"
-            maxLength={25}
-          />          <a
-            href="https://www.naver.com"
-            target="_self"
-          >N</a>
+          <input type="text" placeholder="검색어를 입력하세요" maxLength={25} />{' '}
+          <a href="https://www.naver.com" target="_self">
+            N
+          </a>
         </SearchBox>
 
         {/* 타이머 및 슬라이더 */}
-        <div>{timer}</div>
+        <div className="timer-container">
+          {/* 시간 저장 */}
+          <div>
+            {/* 시간 */}
+            <div>
+              {/* 12시간제로 */}
+              {timer.split(' ')[2].split(':')[0] >= '12'
+                ? parseInt(timer.split(' ')[2].split(':')[0]) - 12
+                : timer.split(' ')[2].split(':')[0]}
+              :{timer.split(' ')[2].split(':')[1]}
+            </div>
+
+            {/* 오전 오후 / 초 */}
+            <div>
+              <div>
+                {timer.split(' ')[2].split(':')[0] >= '12' ? 'PM' : 'AM'}
+              </div>
+
+              <div>{timer.split(' ')[2].split(':')[2]}</div>
+            </div>
+          </div>
+          {/* 날짜 저장 */}
+          <div>
+            {timer.split(' ')[0].split('-')[0]}년
+            {timer.split(' ')[0].split('-')[1]}월
+            {timer.split(' ')[0].split('-')[2]}일
+            {timer.split(' ')[1] === 'Mon' ? '월요일' : ''}
+          </div>
+        </div>
       </div>
 
       {/* 하단 우측 메뉴 + 좌측 카피라이트*/}
-      <div>
-
-      </div>
+      <div></div>
     </BaseContainer>
-  )
+  );
 }
-
 
 const BaseContainer = styled(Container)({
   width: '100%',
   height: '100%',
   minHeight: '100vh',
 
-  backgroundImage: 'url(https://whale-store.pstatic.net/20220712_39/16576213813075atU5_JPEG/386280053685.jpeg)',
+  backgroundImage:
+    'url(https://whale-store.pstatic.net/20220712_39/16576213813075atU5_JPEG/386280053685.jpeg)',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
@@ -69,13 +100,29 @@ const BaseContainer = styled(Container)({
 
   '& .mid-container': {
     width: '100%',
-    height: '50%',
+    height: '100%',
 
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
 
+    '& .timer-container': {
+      width: '100%',
+      height: '100%',
+
+      backgroundColor: 'blue,',
+
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+
+      color: 'white',
+      fontSize: '2rem',
+
+      marginTop: '10%',
+    },
   },
 
   '& .background': {
@@ -86,7 +133,7 @@ const BaseContainer = styled(Container)({
     height: '100vh',
 
     backgroundColor: 'rgba(0, 0, 0, 0.2)',
-  }
+  },
 });
 
 const SearchBox = styled.div({
@@ -148,7 +195,7 @@ const SearchBox = styled.div({
 
       '& + a': {
         color: '#2DB400',
-      }
-    }
-  }
+      },
+    },
+  },
 });
