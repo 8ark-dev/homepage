cee9cf6b485691e8f1ce31c5356118c56804d76a
8ark <sorlti6952@gmail.com>
2023-04-21 12:44:08 +0900

    FECH hover anim
    add left bottom author
    add anim

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,25 @@ import { Container } from '../components/Container';
 import { Timer } from '@/components/Timer';
 
 import styled from '@emotion/styled';
+import Link from 'next/link';
 
 export default function Home() {
 
 
   return (
     <BaseContainer>
+
+      <div className='author'>
+        <div style={
+          {
+            marginBottom: '10px',
+            color: 'white',
+          }
+        }>Summer Memories</div>
+        <div>권희정</div>
+      </div>
+
+
       <div className="background" />
       {/* 상단 우측 사용 의견 */}
       <div></div>
@@ -26,13 +39,12 @@ export default function Home() {
 
         {/* 타이머 및 슬라이더 */}
         <div className="timer-container">
-          <Timer/>
+          <Timer />
         </div>
       </div>
 
       {/* 하단 우측 메뉴 + 좌측 카피라이트*/}
       {/* https://store.whale.naver.com/detail/bec154a88dd44b68adc050eb0e20c00f */}
-      <div></div>
     </BaseContainer>
   );
 }
@@ -89,6 +101,42 @@ const BaseContainer = styled(Container)({
     backgroundColor: 'rgba(0, 0, 0, 0.2)',
     background: 'linear-gradient( to bottom, rgba(0, 0, 0, 0.2) 80%, rgba(0, 0, 0, 0.7) )'
   },
+
+  '& .author': {
+    position: 'absolute',
+    bottom: 40,
+    left: 40,
+
+    textDecoration: 'none',
+    color: 'rgba(255, 255, 255, 0.5)',
+
+    fontWeight: 'lighter',
+    fontSize: '0.8rem',
+
+    zIndex: 1, 
+    
+    transition: 'opacity 2s ease-in-out',
+    //hover 시 opacity 조정
+    '&:hover': {
+      cursor: 'pointer',
+      color: 'rgba(255, 255, 255, 1)',
+    },
+    '&:hover ~ .background': {
+      opacity: 0,
+      transition: 'opacity 1s ease-in-out',
+    },
+    '&:hover ~ .mid-container': {
+      opacity: 0,
+      transition: 'opacity 1s ease-in-out',
+    },
+
+    '& ~ .background': {
+      transition: 'opacity 1s ease-in-out',
+    },
+    '& ~ .mid-container': {
+      transition: 'opacity 1s ease-in-out',
+    },
+  }
 });
 
 const SearchBox = styled.div({
