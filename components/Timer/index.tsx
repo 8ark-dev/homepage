1acf6ca8aaf2052dc5e54156bb26268f4bfa6641
8ark-dev <sorlti6952@gmail.com>
2023-04-26 22:18:23 +0900

    FECH modal input design

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,138 +1,130 @@
-import styled from "@emotion/styled";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import koLocale from "dayjs/locale/ko";
-
+import styled from '@emotion/styled';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import koLocale from 'dayjs/locale/ko';
 
 export const Timer = () => {
-    const [timer, setTimer] = useState(dayjs());
-    useEffect(() => {
-        const counter = setInterval(() => {
-            //format으로 아래에서 해결하기
-            setTimer(dayjs());
-        }, 1000);
+  const [timer, setTimer] = useState(dayjs());
+  useEffect(() => {
+    const counter = setInterval(() => {
+      //format으로 아래에서 해결하기
+      setTimer(dayjs());
+    }, 1000);
 
-        //console.log('timer:', timer.split(' '));
-        return () => {
-            clearInterval(counter);
-        };
-    }, []);
-    return (
-        <TimerContainer>
-            {/* 시간 저장 */}
-            <div className="time">
-                {/* 시간 */}
-                <div className="time-left">
-                    {/* 12시간제로 */}
-                    {timer.format('hh : mm')}
-                </div>
+    //console.log('timer:', timer.split(' '));
+    return () => {
+      clearInterval(counter);
+    };
+  }, []);
+  return (
+    <TimerContainer>
+      {/* 시간 저장 */}
+      <div className="time">
+        {/* 시간 */}
+        <div className="time-left">
+          {/* 12시간제로 */}
+          {timer.format('hh : mm')}
+        </div>
 
-                {/* 오전 오후 / 초 */}
-                <div className="time-right">
-                    <div className="time">
-                        {timer.format('A')}
-                    </div>
+        {/* 오전 오후 / 초 */}
+        <div className="time-right">
+          <div className="time">{timer.format('A')}</div>
 
-                    <div className="not_time"/>
+          <div className="not_time" />
 
-                    <div className="sec time">{timer.format('ss')}</div>
-                </div>
-            </div >
-            {/* 날짜 저장 */}
-            <div className="date">
-                {timer.locale(koLocale).format('YYYY년 MM월 DD일 dddd')}
-            </div >
-        </TimerContainer>
-    )
-}
+          <div className="sec time">{timer.format('ss')}</div>
+        </div>
+      </div>
+      {/* 날짜 저장 */}
+      <div className="date">
+        {timer.locale(koLocale).format('YYYY년 MM월 DD일 dddd')}
+      </div>
+    </TimerContainer>
+  );
+};
 
 const TimerContainer = styled.div({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
 
+  width: '100%',
+  height: '100%',
+
+  zIndex: 1, //배경 이미지보다 위에 있어야 함
+
+  '&:hover': {
+    cursor: 'default',
+  },
+
+  '& .time': {
     width: '100%',
     height: '100%',
 
-    zIndex: 1, //배경 이미지보다 위에 있어야 함
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
 
-    '&:hover': {
-        cursor: 'default',
+    '& .time-left': {
+      width: '25%',
+      height: '100%',
+
+      fontSize: '8rem',
+      fontWeight: 'bold',
+
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
 
-    '& .time': {
+    '& .time-right': {
+      width: '4%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+
+      rowGap: '1rem',
+
+      '& .time': {
         width: '100%',
         height: '100%',
 
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        
-        '& .time-left': {
-            width: '25%',
-            height: '100%',
-            
-            fontSize: '9rem',
-            fontWeight: 'bold',
 
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+        fontWeight: 'light',
+        fontSize: '3rem',
 
-        },
+        color: 'rgba(255,255,255,0.5)',
+      },
 
-        '& .time-right': {
-            width: '4%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            
+      '& .not_time': {
+        width: '100%',
+        height: '1px',
 
-            rowGap: '1rem',
+        backgroundColor: 'rgba(255,255,255,0.3)',
+      },
 
-
-            '& .time': {
-                width: '100%',
-                height: '100%',
-
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-
-
-                fontWeight: 'light',
-                fontSize: '3rem',
-
-                color: 'rgba(255,255,255,0.5)',
-            },
-
-            '& .not_time' :{
-                width: '100%',
-                height: '1px',
-
-                backgroundColor: 'rgba(255,255,255,0.3)',
-            },
-            
-            '& .sec': {
-                width: '100%',
-                height: '100%',
-                fontWeight: 'lighter',
-                color: 'white',
-
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }
-        }
-    },
-
-    '& .date': {
-        fontSize: '1.5rem',
+      '& .sec': {
+        width: '100%',
+        height: '100%',
         fontWeight: 'lighter',
+        color: 'white',
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
     },
+  },
 
-})
-
+  '& .date': {
+    marginTop: '1rem',
+    fontSize: '1.5rem',
+    fontWeight: 'lighter',
+  },
+});
--- components/Todo/modal/index.tsx
+++ components/Todo/modal/index.tsx
@@ -1,62 +1,96 @@
-import styled from '@emotion/styled'
-import React from 'react'
+import styled from '@emotion/styled';
+import React from 'react';
 
 export const TodoModal = () => {
   return (
     <ModalContainer>
-        <p>할 일 목록</p>
+      <p>할 일 목록</p>
 
-        <div>
-            <input 
-                type="text"
-                placeholder="할 일을 입력하세요"
-            />
-        </div>
+      <div>
+        <a>+</a>
+        <input type="text" placeholder="할 일을 입력하세요" />
+      </div>
     </ModalContainer>
-  )
-}
+  );
+};
 
 const ModalContainer = styled.div({
-    width: '460px',
-    height: '350px',
+  width: '460px',
+  height: '350px',
 
-    borderRadius: '20px',
-    boxSizing: 'border-box',
-    padding: '20px',
+  borderRadius: '20px',
+  boxSizing: 'border-box',
+  padding: '20px',
 
-    color: 'black',
+  color: 'black',
 
-    backgroundColor: 'white',
+  backgroundColor: 'white',
 
-    position: 'absolute',
+  position: 'absolute',
 
-    bottom: 35,
-    right: 125,
+  bottom: 35,
+  right: 125,
 
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
 
-    '& input': {
-        width: '100%',
-        height: '100%',
-        border: 'none',
+  '&>p': {
+    width: '100%',
+    height: '30px',
+  },
+
+  '&>div': {
+    width: '100%',
+    height: '30px',
+
+    position: 'relative',
+
+    '&>a': {
+      width: '20px',
+      height: '20px',
+
+      borderRadius: '50%',
+      backgroundColor: '#05c3a7',
+
+      position: 'absolute',
+      left: 10,
+      top: 6,
+
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+
+      color: 'white',
+      fontWeight: 'bolder',
     },
 
-    animation: 'modalOpen 0.1s ease-in-out',
-    '@keyframes modalOpen': {
-        '0%': {
-            width: '0px',
-            height: '0px',
-        },
-        '100%': {
-            width: '460px',
-            height: '350px',
-        }
-    }
+    '& input': {
+      width: '90%',
+      height: '100%',
 
+      border: '2px solid #05c3a7',
+      borderRadius: '20px',
 
+      paddingLeft: '40px',
 
+      color: 'gray',
 
-    
-})
+      '&:focus': {
+        outline: 'none',
+      },
+    },
+  },
+
+  animation: 'modalOpen 0.1s ease-in-out',
+  '@keyframes modalOpen': {
+    '0%': {
+      width: '0px',
+      height: '0px',
+    },
+    '100%': {
+      width: '460px',
+      height: '350px',
+    },
+  },
+});
